95dc44f7466eff45b164ef6f852c23924482aa79
Maryna Soloviova <maryna.soloviova@intellectsoft.net>
2018-12-02 23:43:14 +0200

    add truffle configs and ready contract

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Splitter
+Smart contract named `Splitter` whereby:
+
+* there are 3 people: Alice, Bob and Carol
+* we can see the balance of the Splitter contract on the web page
+* whenever Alice sends ether to the contract, half of it goes to Bob and the other half to Carol
+* we can see the balances of Alice, Bob and Carol on the web page
+* we can send ether to it from the web page
+
+Stretch goals:
+
+* add a kill switch to the whole contract
+* make the contract a utility that can be used by David, Emma and anybody with an address to split Ether between any 2 other addresses of their own choice
+* cover potentially bad input data
---
+++ contracts/Splitter.sol
@@ -0,0 +1,30 @@
+
+pragma solidity ^0.4.24;
+
+contract Splitter  {
+    mapping (address => uint256) balanceOf;
+
+    constructor() public{
+    }
+    
+    event LogSplitTransfer(address indexed sender,  address indexed _payee1,  address indexed _payee2, uint256 amount);
+    
+    function getMsgSender() payable public returns (uint256){
+        return msg.value;
+    }
+    
+    function splitTransfer(address _payee1,  address _payee2) payable public returns (bool success) {
+	require(msg.value > 0);
+        require(balanceOf[msg.sender] >= msg.value);
+        require(_payee1 != msg.sender && _payee2 != msg.sender);
+        require(_payee1 != address(0));
+        require(_payee2 != address(0));
+        
+        uint halfOfTransafer = msg.value/2;
+        balanceOf[_payee1]+= halfOfTransafer;
+        balanceOf[_payee2]+= msg.value - halfOfTransafer;
+        
+        emit LogSplitTransfer(msg.sender, _payee1, _payee2, msg.value);
+        return true;
+    }
+}
